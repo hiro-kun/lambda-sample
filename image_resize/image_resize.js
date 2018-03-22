483099b558b3ef26448c375dcfa0fe85e2af8352
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-03-23 00:19:54 +0900

    fix promise化

--- image_resize/image_resize.js
+++ image_resize/image_resize.js
@@ -3,92 +3,87 @@
 const im = require('imagemagick');
 const fs = require('fs');
 const aws = require('aws-sdk');
+const path = require('path');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-// 最後に呼ばれる
-const postProcessResource = (resource, fn) => {
-    let ret = null;
-    if (resource) {
-        if (fn) {
-            ret = fn(resource);
-        }
-        try {
-            fs.unlinkSync(resource);
-        } catch (err) {
-            // Ignore
-        }
-    }
-    return ret;
-};
+const resize_image = (image_resize_params, context) => new Promise((resolve, reject) => {
 
-
-const resizeImage = (event, context) => {
-
-    // 縮小サイズ決定
-    event.width = 200;
-
-    event.srcData = new Buffer(event.base64Image, 'base64');
-    delete event.base64Image;
     // Lambdaファンクションの動作環境での作業場所
-    event.dstPath = '/tmp/resized.';
+    const dst_path = '/tmp/resized.';
+
+    const im_params = {
+      srcData: new Buffer(image_resize_params.base64_image, 'base64'),
+      dstPath: dst_path,
+      width: image_resize_params.width,
+      height: image_resize_params.height
+    };
 
     try {
-        im.resize(event, (err, stdout, stderr) => {
+        im.resize(im_params, (err, stdout, stderr) => {
             if (err) {
                 throw err;
             } else {
                 console.log('Resize operation completed successfully');
 
-                var params = {
-                    Bucket: event.bucket,
-                    Key: event.outPutName,
-                    Body: new Buffer(fs.readFileSync(event.dstPath)),
-                    ContentType: 'image/jpeg'
-                };
+            const params = {
+                Bucket: image_resize_params.buket_name,
+                Key: image_resize_params.output_file_name,
+                Body: new Buffer(fs.readFileSync(dst_path)),
+                ContentType: 'image/jpeg'
+            };
 
-                // S3にputする
-                s3.putObject(params, (err, data) => {
-                    console.log(err);
-                    console.log(data);
-                    // putが終わったら成功としてLambdaファンクションを閉じる
-                    context.succeed(postProcessResource(event.dstPath, file => {}));
-                });
+                resolve(params);
             }
         });
     } catch (err) {
         console.log('Resize operation failed:', err);
         context.fail(err);
     }
-};
+});
 
-
-// 最初に呼ばれる関数
 exports.handler = (event, context) => {
 
     // S3から渡ってくるバケットの名前の取得
-    event.bucket = event.Records[0].s3.bucket.name;
-
+    const baket_name = event.Records[0].s3.bucket.name;
     // 画像ファイル名取得
-    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    const params = {
-        Bucket: event.bucket,
-        Key: key
-    };
-
-    const reg = /(.*)(?:\.([^.]+$))/;
-    const match =  key.match(reg);
+    const image_file_name = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
 
     // _thumのついているファイルは処理しない(処理するとS3に無限にアップロードされる)
-    if(match[1].lastIndexOf('_thum') != -1){
+    if (image_file_name.lastIndexOf('_resize') != -1) {
         console.log('skip');
         return;
     }
-    // 変換後のファイル名
-    event.outPutName = match[1]+'_thum.'+ match[2];
 
-    // S3のファイルを取得
+    const file_name = path.parse(image_file_name).name;
+    const file_ext = path.parse(image_file_name).ext;
+    // 変換後ファイル名
+    const output_file_name = file_name + '_resize' + file_ext;
+
+    const params = {
+        Bucket: baket_name,
+        Key: image_file_name
+    };
+
+    // S3ファイル名取得
     s3.getObject(params, (err, data) => {
-        event.base64Image = new Buffer(data.Body).toString('base64');
-        resizeImage(event, context);
+        const image_resize_params = {
+            buket_name: baket_name,
+            output_file_name: output_file_name,
+            base64_image: new Buffer(data.Body).toString('base64'),
+            width: 200,
+            height: 200
+        };
+        resize_image(image_resize_params, context)
+            .then((params) => {
+              // S3にputする
+              s3.putObject(params, (err, data) => {
+                  console.log(err);
+                  console.log(data);
+                  resolve("success");
+              });
+            })
+            .then((message) => {
+                context.succeed();
+            });
     });
 };
